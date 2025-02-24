80b37dc8a39f1b5da4ccf7e34a8c27d67e840467
Scott Schumacker <scottschu97@gmail.com>
2025-02-24 17:25:27 -0500

    Updated KPI metrics and added server code to create data set

--- Coffee_Shop_Dashboard/app.R
+++ Coffee_Shop_Dashboard/app.R
@@ -35,7 +35,7 @@ ui <- page_sidebar(
     ),
     value_box(
       title = "Most Popular Item",
-      value = textOutput("change")
+      value = textOutput("mostPopular")
     )
   ),
   card(
@@ -48,6 +48,59 @@ ui <- page_sidebar(
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
+  
+  ##########
+  
+  # Creating data set
+  # Number of transactions
+  set.seed(487)
+  n_transactions <- 1000
+  
+  # Dates (for a month)
+  start_date <- as.Date("2023-10-01")
+  dates <- sample(seq(start_date, start_date + 30, by = "day"), n_transactions, replace = TRUE)
+  
+  # Time of day (morning, afternoon, evening)
+  times <- sample(c("Morning", "Afternoon", "Evening"), n_transactions, replace = TRUE)
+  
+  # Coffee shop items
+  items <- sample(c("Latte", "Cappuccino", "Americano", "Mocha", "Espresso", "Iced Coffee", "Pastry", "Muffin", "Sandwich", "Tea"), n_transactions, replace = TRUE)
+  
+  # Prices (adjust as needed)
+  prices <- numeric(n_transactions)
+  prices[items == "Latte"] <- round(runif(sum(items == "Latte"), 3.5, 5.5), 2)
+  prices[items == "Cappuccino"] <- round(runif(sum(items == "Cappuccino"), 3.5, 5.5), 2)
+  prices[items == "Americano"] <- round(runif(sum(items == "Americano"), 2.5, 4.5), 2)
+  prices[items == "Mocha"] <- round(runif(sum(items == "Mocha"), 4, 6), 2)
+  prices[items == "Espresso"] <- round(runif(sum(items == "Espresso"), 2, 3.5), 2)
+  prices[items == "Iced Coffee"] <- round(runif(sum(items == "Iced Coffee"), 3, 5), 2)
+  prices[items == "Pastry"] <- round(runif(sum(items == "Pastry"), 2, 4), 2)
+  prices[items == "Muffin"] <- round(runif(sum(items == "Muffin"), 2.5, 4.5), 2)
+  prices[items == "Sandwich"] <- round(runif(sum(items == "Sandwich"), 5, 8), 2)
+  prices[items == "Tea"] <- round(runif(sum(items == "Tea"), 2, 4), 2)
+  
+  # Customer ID (optional, for repeat customers)
+  customer_ids <- sample(1:200, n_transactions, replace = TRUE)
+  
+  # Create the data frame
+  coffee_sales <- data.frame(
+    TransactionID = 1:n_transactions,
+    Date = dates,
+    Time = times,
+    ItemID = items,
+    Price = prices,
+    CustomerID = customer_ids
+  )
+  
+  ##########
+  
+  # Creating popular item
+  item_count <- coffee_sales %>% 
+    group_by(ItemID) %>% 
+    summarise(itemCount = n())
+  
+  maxItem <- subset(item_count, itemCount == max(item_count$itemCount),
+                    select = ItemID)
   
   # Creating total revenue data frame
   total_revenueDF <- coffee_sales %>% 
@@ -70,6 +123,10 @@ server <- function(input, output) {
   
   output$totalSalesNum <- renderText({
     totalSalesNumber
+  })
+  
+  output$mostPopular <- renderText({
+    maxItem[[1]]
   })
   
   # Creating total revenue plot
