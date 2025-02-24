7f957dfdfd92f45724f006addd625c6310747da2
Scott Schumacker <scottschu97@gmail.com>
2025-02-24 17:09:56 -0500

    Updated plots and updated side bar to have time frame drop down

--- Coffee_Shop_Dashboard/app.R
+++ Coffee_Shop_Dashboard/app.R
@@ -13,20 +13,25 @@ library(readr)
 # Define UI for application that draws a histogram
 ui <- page_sidebar(
   # App title ----
-  title = "Coffee Sales",
+  title = "Sales Dashboard",
   # Sidebar panel for inputs ----
   sidebar = sidebar(
+    selectInput( 
+      "select", 
+      "Select options below:", 
+      list("October" = "1A", "September" = "1B", "August" = "1C") 
+    ),
   ),
   
   layout_columns(
     fill = FALSE,
     value_box(
       title = "Total Revenue",
-      value = textOutput("total")
+      value = textOutput("totalRevVal")
     ),
     value_box(
       title = "Total Sales",
-      value = textOutput("consume2023")
+      value = textOutput("totalSalesNum")
     ),
     value_box(
       title = "Most Popular Item",
@@ -49,6 +54,24 @@ server <- function(input, output) {
     group_by(Date) %>% 
     summarise(total_rev = sum(Price))
   
+  # Creating total number of sales data frame
+  number_sales <- coffee_sales %>% 
+    group_by(Date) %>% 
+    summarise(count = n())
+  
+  totalRevNumber <- sum(total_revenueDF$total_rev)
+  
+  totalSalesNumber <- sum(number_sales$count)
+  
+  # Creating KPI metrics
+  output$totalRevVal <- renderText({
+    paste0("$ ", round(totalRevNumber,2))
+  })
+  
+  output$totalSalesNum <- renderText({
+    totalSalesNumber
+  })
+  
   # Creating total revenue plot
   output$totalRevPlot <- renderPlotly({
     ggplotly(
@@ -61,11 +84,6 @@ server <- function(input, output) {
         ggtitle("Total Sales Revenue")
     )
   })
-  
-  # Creating total number of sales data frame
-  number_sales <- coffee_sales %>% 
-    group_by(Date) %>% 
-    summarise(count = n())
   
   # Creating total number of sales plot
   output$totalNumberSales <- renderPlotly({
